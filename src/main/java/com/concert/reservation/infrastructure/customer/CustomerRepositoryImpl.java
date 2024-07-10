0016301c0d37b9348b8afc0276d09da6a94a7e52
jmyang <qqs2615@gmail.com>
2024-07-10 19:39:48 +0900

    CustomerRepositoryImpl 파일명 오타 수정

--- src/main/java/com/concert/reservation/infrastructure/customer/CustomerRepositoryImpl.java
+++ src/main/java/com/concert/reservation/infrastructure/customer/CustomerRepositoryImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class CustomerRepositroyImpl implements CustomerRepository {
+public class CustomerRepositoryImpl implements CustomerRepository {
 
     private final CustomerJpaRepository customerJpaRepository;
 
