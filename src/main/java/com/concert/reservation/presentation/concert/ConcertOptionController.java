a305fd82f831d942624ee87b301d196409d238df
jmyang <qqs2615@gmail.com>
2024-07-18 00:02:05 +0900

    콘서트 옵션 컨트롤러 로직 보완

--- src/main/java/com/concert/reservation/presentation/concert/ConcertOptionController.java
+++ src/main/java/com/concert/reservation/presentation/concert/ConcertOptionController.java
@@ -1,11 +1,9 @@
 package com.concert.reservation.presentation.concert;
 
 import com.concert.reservation.application.concert.ConcertFacade;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -22,11 +20,12 @@ public class ConcertOptionController {
      *
      * @author  양종문
      * @since   2024-07-10
+     * @param   concertOptionRequest - 콘서트 옵션 요청
      * @return  List<ConcertOptionResponse>
      */
     @GetMapping("/dates")
-    public List<ConcertOptionResponse> findAllAvailableConcertForReservation() {
-        return ConcertOptionResponse.toResponse(concertFacade.findAllAvailableConcertForReservation());
+    public List<ConcertOptionResponse> findAllAvailableConcertForReservation(@Valid @RequestBody ConcertOptionRequest concertOptionRequest) {
+        return ConcertOptionResponse.toResponse(concertFacade.findAllAvailableConcertForReservation(concertOptionRequest.getCustomerId()));
     }
 
     /**
@@ -36,10 +35,11 @@ public class ConcertOptionController {
      * @since   2024-07-10
      * @param   startDate - 시작일
      * @param   concertOptionId - 콘서트 옵션 ID
+     * @param   concertOptionRequest - 콘서트 옵션 요청
      * @return  List<SeatOptionResponse>
      */
     @GetMapping("/{date}/{concert-option-id}/seats")
-    public List<SeatOptionResponse> findAllAvailableSeatForReservation(@PathVariable("date") LocalDate startDate, @PathVariable("concert-option-id") Long concertOptionId) {
-        return SeatOptionResponse.toResponse(concertFacade.findAllAvailableSeatForReservation(startDate, concertOptionId));
+    public List<SeatOptionResponse> findAllAvailableSeatForReservation(@PathVariable("date") LocalDate startDate, @PathVariable("concert-option-id") Long concertOptionId, @Valid @RequestBody ConcertOptionRequest concertOptionRequest) {
+        return SeatOptionResponse.toResponse(concertFacade.findAllAvailableSeatForReservation(concertOptionRequest.getCustomerId(), startDate, concertOptionId));
     }
 }
